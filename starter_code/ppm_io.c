2593a0a1b13e0bf61a46ec21dc1f912d29a119d5
Sam Lipschitz <slipsch3@jhu.edu>
2020-03-17 15:44:47 -0400

    fixed blended- works when images have same dimensions. Need to check if it works with different dimensions

--- starter_code/ppm_io.c
+++ starter_code/ppm_io.c
@@ -135,7 +135,112 @@ Image * exposure(Image * orig, float ev){
 
 Image * blend(Image * input1, Image * input2, float alpha) {
   Image * new = malloc(sizeof(Image)); //Allocate memory for the new outut Image
-  Pixel *in1p = malloc(sizeof(Pixel) * input1->rows * input1->cols);
+
+  //finding max value of rows and cols
+  int rows = 0;
+  int cols = 0;
+  if(input1->rows > input2->rows){
+    rows = input1->rows;
+  }else{
+    rows = input2->rows;
+  }
+  if(input1->cols > input2->cols){
+    cols = input1->cols;
+  }else{
+    cols = input2->cols;
+  }
+
+  Pixel * pixels = malloc(sizeof(Pixel) * rows * cols);
+  int col1 = 0;
+  int row1 = 0;
+  int col2 = 0;
+  int row2 = 0;
+  int cur_row = 0;
+  int cur_col = 0;
+  int atSingleRows = 0;
+  //Pixel * pixels = malloc(sizeof(Pixel)* rows * cols);
+  new->data = pixels;
+  new -> rows = rows;
+  new-> cols = cols;
+  //TODO: add case for row 0/col 0
+  //looping through each pixel in new image
+  for(int i = 0; i < rows*cols; i++){
+    //Pixel * pixel1 = malloc(sizeof(Pixel));
+    //Pixel * pixel2 = malloc(sizeof(Pixel));
+    /*Pixel * pixels = malloc(sizeof(Pixel)* rows * cols);
+    new->data = pixels;
+    new -> rows = rows;
+    new-> cols = cols;*/
+    //if the rows/cols match up for both images simply use given calculation
+    if((row1==row2) && (col1 == col2)){
+      if(alpha*input1->data[i].r + (1-alpha)*input2->data[i].r > 255){
+	new->data[i].r = 255;
+      }else{
+	new->data[i].r = alpha*input1->data[i].r + (1-alpha)*input2->data[i].r;
+      }
+      if(alpha*input1->data[i].g + (1-alpha)*input2->data[i].g > 255){
+	new->data[i].g = 255;
+      }else{
+	new->data[i].g = alpha*input1->data[i].g + (1-alpha)*input2->data[i].g;
+      }
+      if(alpha*input1->data[i].b + (1-alpha)*input2->data[i].b > 255){
+	new->data[i].b = 255;
+      }else{
+	new->data[i].b = alpha*input1->data[i].b + (1-alpha)*input2->data[i].b;
+      }
+      //finding "coordinate" of pixel from each input
+      if(i+1 % input1->cols == 0){
+	row1 += 1;
+	col1 = 0;
+      }else{
+	col1+=1;
+      }
+      if(i+1 % input2->cols == 0){
+	row2 += 1;
+	col2 = 0;
+      }else{
+	col2+=1;
+      }
+    }/*else{
+      if(col1 +1 > input2->cols){//if at a col that doesn't exist for input2
+	new->data[i].r = input1->data[(row1*input1->cols)+col1].r;
+	new->data[i].g = input1->data[(row1*input1->cols)+col1].g;
+	new->data[i].b = input1->data[(row1*input1->cols)+col1].b;
+	if(i+1 % input1->cols == 0 && row1 +1 < input1->rows){//if at end of a col and another row exists for input1
+	  row1 += 1;
+	  col1 = 0;
+	}else if(row1 +1 < input1->rows){//if not at end of col
+	  col1+=1;
+	}
+	else{//at last row of input1 
+	  row2 += 1;
+	  col2 = 0;
+	  atSingleRows = 1;
+	}
+      }else if(col1 == 0 && atSingleRows == 0){//if just started new col for input1 (where input1 has bigger col)
+	col2 = 0;
+	row2 = row1;
+      }
+      if(atSingleRows == 1 && row2 + 1 < input2->rows){
+	new->data[i].r = input2->data[(row2*input2->cols)+col2].r;
+	new->data[i].g = input2->data[(row2*input2->cols)+col2].g;
+	new->data[i].b = input2->data[(row2*input2->cols)+col2].b;
+	if(i+1 % input2->cols == 0){
+	  row2 += 1;
+	  col2 = 0;
+	}else{
+	  col2+=1;
+	}
+      }
+      else if(atSingleRows == 1){
+	new->data[i].r = 0;
+	new->data[i].g = 0;
+	new->data[i].b = 0;
+	printf("fuck");
+      }
+    }*/ 
+  }
+  /*Pixel *in1p = malloc(sizeof(Pixel) * input1->rows * input1->cols);
   Pixel *in2p = malloc(sizeof(Pixel) * input2->rows * input2->cols);
   new->data = in1p; //Setting base pixels as image 1
   new->rows = in1p->rows;
@@ -159,7 +264,8 @@ Image * blend(Image * input1, Image * input2, float alpha) {
       new->data[i].b = b;
     }
   free(in1p);
-  free(in2p);
+  free(in2p);*/
+  
   return new;
 }
   
