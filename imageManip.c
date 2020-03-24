146984e107c6d4a88a986064ed2c135b8a9e04d7
Sam Lipschitz <slipsch3@jhu.edu>
2020-03-24 11:24:36 -0400

    removed README.md

--- imageManip.c
+++ imageManip.c
@@ -555,9 +555,9 @@ int doOperation(char *argv[]){
   if(strcmp(argv[3], "blur") == 0){
     double sigma;
     sscanf(argv[4], "%lf", &sigma);
-    //outputI = blur(inputI, sigma);
-    outputI = inputI;
-    skip = 1;
+    outputI = blur(inputI, sigma);
+    //outputI = inputI;
+    //skip = 1;
   }
   
   if(write_ppm(outputF, outputI) == -1){
